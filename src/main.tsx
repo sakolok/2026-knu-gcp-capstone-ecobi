9099e9800d9c7ae1f6f748122fff75947d4cd055
kolok91 <36368889+sakolok@users.noreply.github.com>
2026-06-12 17:33:40 +0900

    chore: remove internal ERD artifacts

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "font-awesome/css/font-awesome.min.css";
 import "aos/dist/aos.css";
-import "../mvp/styles.css";
 import "./styles/app.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
